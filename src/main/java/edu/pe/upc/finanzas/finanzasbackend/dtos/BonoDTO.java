fd4eabfd0861202a09f3a338afaa68965efe61ae
OmBRz <omarbrrcl@gmail.com>
2025-07-09 02:26:20 -0500

    feat: add Lombok annotations to Bono and BonoDTO for cleaner code.

--- src/main/java/edu/pe/upc/finanzas/finanzasbackend/dtos/BonoDTO.java
+++ src/main/java/edu/pe/upc/finanzas/finanzasbackend/dtos/BonoDTO.java
@@ -1,12 +1,13 @@
 package edu.pe.upc.finanzas.finanzasbackend.dtos;
 
 import jakarta.persistence.Column;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
 public class BonoDTO {
-    private Long bonoId;
     private Double valorNominal;
     private Double valorComercial;
     private Long numAnios;
@@ -20,107 +21,4 @@ public class BonoDTO {
     private BigDecimal porcCavali;
     private BigDecimal inflacionAnual;
 
-    public Long getBonoId() {
-        return bonoId;
-    }
-
-    public void setBonoId(Long bonoId) {
-        this.bonoId = bonoId;
-    }
-
-    public Double getValorNominal() {
-        return valorNominal;
-    }
-
-    public void setValorNominal(Double valorNominal) {
-        this.valorNominal = valorNominal;
-    }
-
-    public Double getValorComercial() {
-        return valorComercial;
-    }
-
-    public void setValorComercial(Double valorComercial) {
-        this.valorComercial = valorComercial;
-    }
-
-    public Long getNumAnios() {
-        return numAnios;
-    }
-
-    public void setNumAnios(Long numAnios) {
-        this.numAnios = numAnios;
-    }
-
-    public BigDecimal getTasaInteresPorc() {
-        return tasaInteresPorc;
-    }
-
-    public void setTasaInteresPorc(BigDecimal tasaInteresPorc) {
-        this.tasaInteresPorc = tasaInteresPorc;
-    }
-
-    public BigDecimal getTasaAnualDesc() {
-        return tasaAnualDesc;
-    }
-
-    public void setTasaAnualDesc(BigDecimal tasaAnualDesc) {
-        this.tasaAnualDesc = tasaAnualDesc;
-    }
-
-    public Long getImpRenta() {
-        return impRenta;
-    }
-
-    public void setImpRenta(Long impRenta) {
-        this.impRenta = impRenta;
-    }
-
-    public LocalDate getFechaEmision() {
-        return fechaEmision;
-    }
-
-    public void setFechaEmision(LocalDate fechaEmision) {
-        this.fechaEmision = fechaEmision;
-    }
-
-    public BigDecimal getPorcEstructuracion() {
-        return porcEstructuracion;
-    }
-
-    public void setPorcEstructuracion(BigDecimal porcEstructuracion) {
-        this.porcEstructuracion = porcEstructuracion;
-    }
-
-    public BigDecimal getPorcColocacion() {
-        return porcColocacion;
-    }
-
-    public void setPorcColocacion(BigDecimal porcColocacion) {
-        this.porcColocacion = porcColocacion;
-    }
-
-    public BigDecimal getPorcFlotacion() {
-        return porcFlotacion;
-    }
-
-    public void setPorcFlotacion(BigDecimal porcFlotacion) {
-        this.porcFlotacion = porcFlotacion;
-    }
-
-    public BigDecimal getPorcCavali() {
-        return porcCavali;
-    }
-
-    public void setPorcCavali(BigDecimal porcCavali) {
-        this.porcCavali = porcCavali;
-    }
-
-    public BigDecimal getInflacionAnual() {
-        return inflacionAnual;
-    }
-
-    public void setInflacionAnual(BigDecimal inflacionAnual) {
-        this.inflacionAnual = inflacionAnual;
-    }
 }
