2fbc191c044891a3123d3734976002724fffa64b
OmBRz <omarbrrcl@gmail.com>
2025-07-09 15:06:19 -0500

    feat(tipo-capitalizacion): implement create method and update listarTipos to return ResponseEntity.

--- src/main/java/edu/pe/upc/finanzas/finanzasbackend/serviceinterfaces/ITipoCapitalizacionService.java
+++ src/main/java/edu/pe/upc/finanzas/finanzasbackend/serviceinterfaces/ITipoCapitalizacionService.java
@@ -1,8 +1,10 @@
 package edu.pe.upc.finanzas.finanzasbackend.serviceinterfaces;
 
+import edu.pe.upc.finanzas.finanzasbackend.dtos.TipoCapitalizacionDTO;
 import edu.pe.upc.finanzas.finanzasbackend.entities.TipoCapitalizacion;
 import java.util.List;
 
 public interface ITipoCapitalizacionService {
     List<TipoCapitalizacion> list();
+    TipoCapitalizacion create(TipoCapitalizacionDTO tipoCapitalizacionDTO);
 }
