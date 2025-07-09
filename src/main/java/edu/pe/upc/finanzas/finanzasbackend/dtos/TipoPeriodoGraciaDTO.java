6cc0aaab1fbde08903e86265ebdd9051245f9a22
OmBRz <omarbrrcl@gmail.com>
2025-07-09 15:06:36 -0500

    feat(tipo-periodo-gracia): implement create method and update listarTipos to return ResponseEntity

--- src/main/java/edu/pe/upc/finanzas/finanzasbackend/dtos/TipoPeriodoGraciaDTO.java
+++ src/main/java/edu/pe/upc/finanzas/finanzasbackend/dtos/TipoPeriodoGraciaDTO.java
@@ -1,16 +1,8 @@
 package edu.pe.upc.finanzas.finanzasbackend.dtos;
 
 public class TipoPeriodoGraciaDTO {
-    private Long graciaId;
     private String descripcion;
 
-    public Long getGraciaId() {
-        return graciaId;
-    }
-
-    public void setGraciaId(Long graciaId) {
-        this.graciaId = graciaId;
-    }
 
     public String getDescripcion() {
         return descripcion;
